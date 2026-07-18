7a90c7be1502468b09498e2417e1b540d5b2be65
Tasnim A <tasnimabdi13@outlook.com>
2026-07-18 23:17:42 +0200

    this is the home page layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-//import HomePage from './pages/HomePage';
+import HomePage from "./pages/HomePage.tsx";
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-import HomePage from './pages/HomePage';
 //import DashboardPage from './pages/DashboardPage';
 //import NotFoundPage from './pages/NotFoundPage';
 
@@ -19,9 +18,9 @@ function App() {
   return (
     <BrowserRouter> {/*routes to different pages based on path in the url*/}
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
