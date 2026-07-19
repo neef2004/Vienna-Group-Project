f558e3b9934ed4b001e76ea131b052055becc15d
Hanif Ahadi <hanifthings@gmail.com>
2026-07-19 19:43:14 +0200

    fixing styling issues after merging landing page branch with itinerary frontend branch

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import "./styles/App.css";
-/* import "./styles/HomePage.css"; */
+import "./styles/Auth.css";
+import "./styles/HomePage.css";
 import 'bootstrap/dist/css/bootstrap.css'
 
 createRoot(document.getElementById('root')!).render(
